d4c403c4878b842ab2038db32a5b57331b463304
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-11-24 14:38:42 -0800

    deploy as static

--- components/SyntaxTab.tsx
+++ components/SyntaxTab.tsx
@@ -1,12 +1,7 @@
 import { Tab } from "fumadocs-ui/components/tabs";
 import type { SyntaxKind } from "./utils.js";
-import { CodeBlock } from "fumadocs-ui/components/codeblock";
 
 export const SyntaxTab: React.FC<{ children: string; kind: SyntaxKind }> = ({
   children,
   kind,
-}) => (
-  <Tab value={kind}>
-    <CodeBlock>{children}</CodeBlock>
-  </Tab>
-);
+}) => <Tab value={kind}>{children}</Tab>;
